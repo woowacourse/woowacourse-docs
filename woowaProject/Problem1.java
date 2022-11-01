a0a59f6894656f34e5a477a28aee050435f5241d
홍예석 <yshong1998@gmail.com>
2022-11-01 16:12:16 +0900

    message

---
+++ woowaProject/Problem1.java
@@ -0,0 +1,8 @@
+package woowaProject;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a);
+    }
+}
---
+++ woowaProject/Problem3.java
@@ -0,0 +1,4 @@
+package woowaProject;
+
+public class Problem3 {
+}
